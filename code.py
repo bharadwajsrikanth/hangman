b0c72e5d601ed773f8f6de9cea591feb3e19b0c2
Srikanth Bharadwaj <bharadwajs@ufl.edu>
2020-01-15 12:18:32 +0530

    Introducing genre selection feature. Player can chose among 2 genres. Also includes bug fixes.

--- code.py
+++ code.py
@@ -1,3 +1,12 @@
+from enum import Enum
+import csv
+import random
+
+class Genres(Enum):
+    Movies = 1
+    Sports = 2
+
+
 def play_hangman(word):
     won = False
     wrong = 0
@@ -17,15 +26,19 @@ def play_hangman(word):
         print("\nPrevious Guesses: {0}".format(" ".join(guessed_letters)))
         print("\n".join(stages[0:wrong+1]))
 
+    def find_occurrences(list, ch):
+        return [i for i, x in enumerate(list) if x == ch]
+
     show_progress()
     while wrong < len(stages) - 1 and not won:
-        msg = "\nChoose the next letter: "
+        msg = "\nChoose a letter: "
         chosen_letter = input(msg)
         guessed_letters.append(chosen_letter)
         if chosen_letter in letters:
-            char_idx = letters.index(chosen_letter)
-            board[char_idx] = chosen_letter
-            letters[char_idx] = "$"
+            indices = find_occurrences(letters, chosen_letter)
+            for idx in indices:
+                board[idx] = chosen_letter
+                letters[idx] = "$"
         else:
             wrong = wrong + 1
         show_progress()
@@ -38,6 +51,34 @@ def play_hangman(word):
         print("The word is: {0}".format(word))
 
 
+def get_genre():
+    print("\nAvailable Genres")
+    for i in range(len(Genres)):
+        print("{0}. {1}".format(i+1, Genres(i+1).name))
+    msg = "\nChoose a genre: "
+    return input(msg)
+
+
+def get_word(genre):
+    movies_filename = 'movies'
+    sports_filename = 'sports'
+
+    if genre == 1:
+        filename = movies_filename + '.csv'
+    elif genre == 2:
+        filename = sports_filename + '.csv'
+
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for lines in csv_reader:
+            words = lines
+
+    return random.choice(words)
+
+
+
 if __name__ == "__main__":
-    given_word = "python"
+    selected_genre = int(get_genre())
+    print("\nSelecting a word from {0} genre".format(Genres(selected_genre).name))
+    given_word = get_word(selected_genre)
     play_hangman(given_word)
